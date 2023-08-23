e8625ed93b59674dd18fcc0918250aa7bcafa4f5
Моисеев Роман Игоревич <programmer1@mscontact.local>
2023-08-23 16:18:33 +0300

    поправлен вывод проектов

--- resources/views/main_page/projectMenu/main.blade.php
+++ resources/views/main_page/projectMenu/main.blade.php
@@ -2,6 +2,7 @@
 
     <div class="container" style = 'border: thick double #32a1ce;margin-left:71%; margin-top:-40%; max-width:30%; height:400px'>
         <div class="container" >
+            
             <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                  Создать проект
             </button>
