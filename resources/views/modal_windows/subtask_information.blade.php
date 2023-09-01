d8a5c384b913d671742d4187796e0e4952c130c8
Моисеев Роман Игоревич <programmer1@mscontact.local>
2023-09-01 14:15:22 +0300

    Готово модально окно подзадачи, но не готова его сохранение

--- resources/views/modal_windows/subtask_information.blade.php
+++ resources/views/modal_windows/subtask_information.blade.php
@@ -8,7 +8,42 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Сотрудник</label>
+                                <select class="form-select" aria-label="Пример выбора по умолчанию">
+                                    <option selected>
+                                        {{$users->where('id', $participant->where('id', $_GET['participant'])->first()->participant_id)->first()->name}}
+                                    </option>
+                                    @foreach($phase_participants as $key => $phase_participants)
+                                        @if($phase_participants->participant_id != $users->where('id', $participant->where('id', $_GET['participant'])->first()->participant_id)->first()->id)
+                                            <option>
+                                                {{$users->where('id', $participant->where('id', $phase_participants->participant_id)->first()->participant_id)->first()->name}}
+                                            </option>
+                                        @endif
+                                    @endforeach
+                                </select>
+                        </div>
+                        <div class="mb-3">
+                            <div class="input-group">
+                                <span class="input-group-text">Описание задачи</span>
+                                <textarea class="form-control" aria-label="С текстовым полем">{{$participant->where('id', $_GET['participant'])->first()->comment}}</textarea>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Дата начала задачи</label>
+                            <input type="date" class="form-control" value='{{date('Y-m-d', strtotime($participant->time_frome))}}'>
+                            <label for="exampleInputPassword1" class="form-label">Время начала задачи</label>
+                            <input type="time" class="form-control" value='{{date('H:i:s', strtotime($participant->time_frome))}}'>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Дата закрытия задачи</label>
+                            <input type="date" class="form-control" value='{{date('Y-m-d', strtotime($participant->time_to))}}'>
+                            <label for="exampleInputPassword1" class="form-label">Время закрытия задачи</label>
+                            <input type="time" class="form-control" value='{{date('H:i:s', strtotime($participant->time_to))}}'>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Отправить</button>
+                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
