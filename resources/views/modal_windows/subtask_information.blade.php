6beeb6a226ed003180d22634689fd6b684b7ff28
Моисеев Роман Игоревич <programmer1@mscontact.local>
2023-09-06 09:08:11 +0300

    Изменено слово задача на подзадача, в меню подзадач проекта

--- resources/views/modal_windows/subtask_information.blade.php
+++ resources/views/modal_windows/subtask_information.blade.php
@@ -91,7 +91,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Новая задача</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Новая подзадача</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть" onclick='closeModal()'></button>
                 </div>
                 <div class="modal-body">
